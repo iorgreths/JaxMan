36fa73c83b97623982f829edcf136c5f23bd8084
Iorgreths <m.gredler33@gmail.com>
2014-01-18 16:04:01 +0100

    Create SoundClip(1.0)

---
+++ src/IO/SoundClip.java
@@ -0,0 +1,13 @@
+package IO;
+
+/**
+ * 
+ * A Class that represents a SoundClip, which can be played, stopped, looped, etc.
+ * 
+ * @author Iorgreths
+ * @version 1.0
+ *
+ */
+public class SoundClip {
+
+}
