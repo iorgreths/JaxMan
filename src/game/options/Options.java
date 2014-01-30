858b26b367835c80097a8224595a247a7f43f48e
Iorgreths <m.gredler33@gmail.com>
2014-01-30 21:09:06 +0100

    Options == 1 Instance

--- src/game/options/Options.java
+++ src/game/options/Options.java
@@ -1,5 +1,23 @@
 package game.options;
 
 public class Options {
+	
+	private static Options instance;
+	
+	private Options(){
+		
+	}
 
+	public static Options getInstance(){
+		
+		if(instance==null){
+		
+			instance=new Options();
+		
+		}
+		
+		return instance;
+		
+	}
+	
 }
---
+++ src/game/options/PlayerProp.java
@@ -0,0 +1,5 @@
+package game.options;
+
+class PlayerProp {
+
+}
