cf5b47c7da510e9e2830b93a56ca8d774d31c3cf
Maxmanski <maxmanski@live.at>
2014-01-30 22:53:44 +0100

    update io/graphics

--- src/io/graphics/GameFrame.java
+++ src/io/graphics/GameFrame.java
@@ -2,6 +2,8 @@ package io.graphics;
 
 import game.options.Options;
 
+import java.awt.Canvas;
+import java.awt.Component;
 import java.awt.DisplayMode;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
@@ -9,16 +11,18 @@ import java.awt.GraphicsEnvironment;
 import javax.swing.JFrame;
 
 public class GameFrame extends JFrame{
+	private static final long serialVersionUID = -7489048496141498327L;
 	private final GraphicsDevice dev;
 	private final DisplayMode dm;
 	private final int width, height;
 	private static GameFrame instance;
+	private Canvas canvas;
 
 	/**
 	 * 
 	 * @param fullscreen
 	 */
-	private GameFrame(boolean fullscreen){
+	private GameFrame(boolean fullscreen, Canvas canvas){
 		this.dev = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
 		this.dm = dev.getDisplayMode();
 		Options o = Options.getInstance();
@@ -40,9 +44,23 @@ public class GameFrame extends JFrame{
 			this.setUndecorated(false);
 		}
 
+		if(canvas!=null){
+			canvas.setFocusable(false);
+			this.getContentPane().add(canvas);
+		}
+		
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setVisible(true);
 		this.requestFocus();
 		GameFrame.instance = this;
+	}
+	
+	/**
+	 * 
+	 * @param fullscreen
+	 */
+	private GameFrame(boolean fullscreen){
+		this(fullscreen, null);
 	}
 
 	/**
@@ -50,13 +68,14 @@ public class GameFrame extends JFrame{
 	 * @param fullscreen
 	 * @return
 	 */
-	public static GameFrame setFullscreen(boolean fullscreen){
+	public GameFrame setFullscreen(boolean fullscreen){
 		if(GameFrame.instance!=null){
 			instance.setVisible(false);
 			instance.dispose();
 		}
 
 		GameFrame.instance = new GameFrame(fullscreen);
+		GameFrame.instance.requestFocus();
 		return GameFrame.instance;
 	}
 
@@ -71,20 +90,46 @@ public class GameFrame extends JFrame{
 		}
 		return GameFrame.instance;
 	}
-	
+
 	/**
 	 * 
 	 */
 	public void dispose(){
 		super.dispose();
-		this.instance=null;
+		GameFrame.instance=null;
 	}
 
-	public static void main(String...strings ) throws InterruptedException{
-		Thread.sleep(1000);
-		GameFrame.setFullscreen(true);
-		Thread.sleep(1000);
-		GameFrame.setFullscreen(false);
+	/**
+	 * 
+	 * @return
+	 */
+	public boolean isFullscreen(){
+		return dev.getFullScreenWindow()!=null;
+	}
+
+	/**
+	 * 
+	 */
+	public void setCanvas(Canvas canvas){
+		if(this.canvas!=null){
+			boolean contains = false;
+			
+			for(Component c: this.getContentPane().getComponents()){
+				if(c.equals(this.canvas)){
+					contains = true;
+					break;
+				}
+			}
+			
+			if(contains){
+				this.getContentPane().remove(this.canvas);
+			}
+		}
+		
+		canvas.setFocusable(false);
+		this.getContentPane().add(canvas);
+		this.canvas = canvas;
+		this.requestFocus();
 	}
 
 }
