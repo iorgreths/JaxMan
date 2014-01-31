35ad67fd1622edd2591a47705c741d8cd980fd23
Iorgreths <m.gredler33@gmail.com>
2014-01-31 11:06:16 +0100

    + comments (Observer, Observable)

--- src/game/runtime/Observable.java
+++ src/game/runtime/Observable.java
@@ -1,15 +1,22 @@
 package game.runtime;
 
+/**
+ * An interface representing the connections towards 'Observable' objects
+ * 
+ * @author Maxmanski, Iorgreths
+ *
+ */
 public interface Observable {
 
 	/**
+	 * Adds an observer to the list of observers of this object.
 	 * 
-	 * @param o
+	 * @param o - The object, which shall observe this object.
 	 */
 	public void addObserver(Observer o);
 	
 	/**
-	 * 
+	 * Notifies all current observers, that something has changed in this object.
 	 */
 	public void notifyObservers();
 }
--- src/game/runtime/Observer.java
+++ src/game/runtime/Observer.java
@@ -1,6 +1,18 @@
 package game.runtime;
 
+/**
+ * An interface representing the connection towards objects, which can 'Observer'
+ * other objects.
+ * 
+ * @author Maxmanski, Iorgreths
+ *
+ */
 public interface Observer {
 
+	/**
+	 * Notifies this observer about an change in one of it's observed objects.
+	 * @param object - The objects which changed.
+	 */
+	public void update(Observable object);
 	
 }
