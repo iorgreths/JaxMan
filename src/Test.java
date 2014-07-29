19d7614c6930fdfe488ec72770d419f3b49d6813
Maxmanski <maxmanski@live.at>
2014-07-29 18:48:14 +0200

    test update

--- src/Test.java
+++ src/Test.java
@@ -6,32 +6,47 @@ import io.graphics.GameFrame;
 
 import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Shape;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
+import java.awt.geom.Ellipse2D;
 import java.awt.image.BufferStrategy;
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class Test {
+
+	public static List<Shape> shapeList;
+	public static int offsetX=0, offsetY=0;
+
 	public static void main(String[] args) throws InterruptedException, DeadInstanceException{
 
+		List<Shape> tmp;
+		Test.shapeList = new ArrayList<Shape>();
 		Game game = new TestGame();
 		GameFrame gf = game.getFrame();
 		gf.setVisible(true);
 
 		BufferStrategy strat = gf.getBufferStrategy();
 		Dimension d = gf.getCanvasDimension();
-		Graphics g;
+		Graphics2D g;
 
 		Thread.sleep(500);
 
 		for(int i=255; i >= 0; i--){
-			g = strat.getDrawGraphics();
+			synchronized(Test.class){
+				tmp = new ArrayList<>(shapeList);
+			}
+			g = (Graphics2D)strat.getDrawGraphics();
 			g.setColor(new Color(i,i,i));
 			g.fillRect(0, 0, d.width, d.height);
 			g.setColor(Color.black);
-			g.drawString("Good Day Sir", d.width/2, d.height/2);
+			g.drawString("Good Day Sir", d.width/2 + Test.offsetX, d.height/2 + Test.offsetY);
+			for(Shape s: tmp){
+				g.draw(s);
+				g.fill(s);
+			}
 			g.dispose();
 			strat.show();
 			Thread.sleep(5);
@@ -46,11 +61,19 @@ public class Test {
 		d = gf.getCanvasDimension();
 
 		for(int i = 0; i < 256; i++){
-			g = strat.getDrawGraphics();
+			synchronized(Test.class){
+				tmp = new ArrayList<>(shapeList);
+			}
+			g = (Graphics2D)strat.getDrawGraphics();
 			g.setColor(new Color(i, i, i));
 			g.fillRect(0, 0, d.width, d.height);
 			g.setColor(Color.black);
-			g.drawString("I said GOOD DAY SIR", d.width/2, d.height/2);
+			g.drawString("I said GOOD DAY SIR", d.width/2 + Test.offsetX, d.height/2 + Test.offsetY);
+
+			for(Shape s: tmp){
+				g.draw(s);
+				g.fill(s);
+			}
 			g.dispose();
 			strat.show();
 			Thread.sleep(5);
@@ -79,65 +102,66 @@ class TestGame extends Game{
 	}
 
 	public void gameLoop() {
-		
+
 
 	}
 
 	@Override
 	public void mouseDragged(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		BufferStrategy b;
-		try {
-			b = GameFrame.getInstance().getBufferStrategy();
-			Graphics2D g = (Graphics2D) b.getDrawGraphics();
-			g.setColor(Color.blue);
-			g.drawOval(e.getX(), e.getY(), 50, 50);
-			g.dispose();
-			b.show();
-		} catch (DeadInstanceException e1) {
-			e1.printStackTrace();
-		}
 	}
 
 	@Override
-	public void mousePressed(MouseEvent e) {
-		// TODO Auto-generated method stub
-		
+	public synchronized void mousePressed(MouseEvent e) {
+		Test.shapeList.add(new Ellipse2D.Double(e.getX(), e.getY(), 10, 10));
+
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void mouseMoved(MouseEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
 		if(e.getKeyCode() == KeyEvent.VK_ESCAPE){
 			System.exit(0);
+		}else if(e.getKeyCode() == KeyEvent.VK_UP){
+			Test.offsetY -= 5;
+			
+		}else if(e.getKeyCode() == KeyEvent.VK_LEFT){
+			Test.offsetX -= 5;
+			
+		}else if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+			Test.offsetX += 5;
+			
+		}else if(e.getKeyCode() == KeyEvent.VK_DOWN){
+			Test.offsetY += 5;
+			
 		}
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 	@Override
 	public void keyTyped(KeyEvent e) {
-		
+
 	}
 
 }
